a05f55802e4e2f16f9bfa08ffa21baff253e255e
Davide22-sy <davide.valerio0201@gmail.com>
2025-01-25 14:38:17 +0100

    modifica acquisto e icona

--- js/navbar.js
+++ js/navbar.js
@@ -4,4 +4,12 @@ window.onload = () => {
     const numberCredits = currentUser.numberCredits // quanti crediti il currentUser ha a disposizione
     const divCrediti = document.getElementById("ncrediti"); //div che contiene il numero dei cr
     divCrediti.innerText = numberCredits
+
+    //codice per rendere icona + interattiva con rimando all'acquisto
+    const buyIcon = document.querySelector(".buy-icon");
+    if (buyIcon) {
+        buyIcon.addEventListener("click", () => {
+            window.location.href = "crediti.html";
+        })
+    }
 }
