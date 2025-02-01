0cd8f738d0550154e471ecb6e5d0bf83059ef314
Davide22-sy <davide.valerio0201@gmail.com>
2025-02-01 12:32:28 +0100

    bast

--- js/navbar.js
+++ js/navbar.js
@@ -4,9 +4,11 @@ window.addEventListener("load", () => {
     console.log("window.onload funziona")
     const currentUser = JSON.parse(localStorage.getItem("currentUser")); //recupero dei dati dell'utente corrente
     const numberCredits = currentUser.numberCredits // quanti crediti il currentUser ha a disposizione
-    const divCrediti = document.getElementById("ncrediti"); //div che contiene il numero dei cr
-    divCrediti.innerText = numberCredits
-
+    const divCrediti = document.getElementsByClassName("ncrediti"); //div che contiene il numero dei cr
+    divCrediti.forEach(element => {
+        element.innerText = numberCredits     
+    });
+    
     //codice per rendere icona + interattiva con rimando all'acquisto
     const buyIcon = document.querySelector(".buy-icon");
     if (buyIcon) {
@@ -14,6 +16,4 @@ window.addEventListener("load", () => {
             window.location.href = "crediti.html";
         })
     }
-    console.log("fine funzio funzia")
-
 })
