e63992d9761d2fac8a4b5f55eb12c2b98a2165fc
Davide22-sy <davide.valerio0201@gmail.com>
2025-02-01 11:08:37 +0100

    js acquisto pacchetti

---
+++ js/acquistopack.js
@@ -0,0 +1,6 @@
+window.onload = () => {
+    const buyPackBtn = document.getElementById("btn-acquistopack");
+    buyPackBtn.addEventListener("click", function() {
+        console.log("Pulsante 'acquista pacchetti' cliccato");
+    });
+}
