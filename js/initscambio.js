1469624caedbe9bccc5a7755e657e1e4665214b5
Davide22-sys <davide.valerio0201@gmail.com>
2025-02-11 23:10:45 +0100

    modale

--- js/initscambio.js
+++ js/initscambio.js
@@ -1,3 +1,30 @@
 const figurineClickHandler = card => {
-    console.log(card);
+    const modale = document.getElementById("modale-sceltauser"); 
+    modale.dataset.name = card.dataset.name;
+    modale.style.display = "block";
 }
+document.addEventListener ("DOMContentLoaded", function(){
+    const modale = document.getElementById("modale-sceltauser"); 
+    const closeSpan = modale ? modale.querySelector(".close") : null; 
+    // X per chiudere la modale
+
+    /**
+     * Funzione per chiudere la modale
+     */
+    function chiudiModale() {
+        if (modale) modale.style.display = "none";
+    }
+    // Quando clicco sulla "X", chiudo la modale
+    if (closeSpan) {
+        closeSpan.addEventListener("click", chiudiModale);
+    }
+    
+    // Quando clicco fuori dalla modale, chiudo la modale
+    window.addEventListener("click", (event) => {
+        if (event.target === modale) {
+            chiudiModale();
+        }
+    });
+    
+
+})
