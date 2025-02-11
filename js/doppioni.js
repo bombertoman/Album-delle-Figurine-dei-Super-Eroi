64f80586859ec2f5d7a252eb36895de9e4868c41
Davide22-sys <davide.valerio0201@gmail.com>
2025-02-11 22:06:20 +0100

    doppioni.js creazione

---
+++ js/doppioni.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const divDoppioni = document.getElementById("doppioni");
+    const figurines = getCurrentUserItem("figurines");
+    const doppioni = [];
+    const nomiFigurineUnivoche = [];
+    figurines.forEach(figurina => {
+        if (nomiFigurineUnivoche.includes(figurina.name)){
+            const doppioneTrovato = doppioni.some(doppione => {
+                return doppione.name === figurina.name;
+            })
+            if (doppioneTrovato) {
+                doppioneTrovato.count = doppioneTrovato.count + 1;
+                return; 
+            }
+            doppioni.push({count: 1, name: figurina.name});
+            return;
+        }
+        nomiFigurineUnivoche.push(figurina.name); 
+    });
+    console.log(doppioni);
+})
