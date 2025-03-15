9c1f8a4b548a2e3e8dbd560af51240639b45b3da
Davide22-sys <davide.valerio0201@gmail.com>
2025-03-15 11:50:37 +0100

    let

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,6 @@
 const acc = document.getElementsByClassName("accordion");
 
-for (const i = 0; i < acc.length; i++) {
+for (let i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function () {
     /* Toggle between adding and removing the "active" class,
     to highlight the button that controls the panel */
