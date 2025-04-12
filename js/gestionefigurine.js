74a720d52b6dd858af63dbe05d46f1b13b8d9717
Davide22-sys <davide.valerio0201@gmail.com>
2025-04-12 11:17:43 +0200

    modifiche html per visualizzazione fig

--- js/gestionefigurine.js
+++ js/gestionefigurine.js
@@ -21,6 +21,7 @@ function aggiornaAlbumInHtml(figurines) {
   figurines.forEach(fig => {
     const card = visualizzaFigurina(fig, albumContainer)
     card.addEventListener("click", () => console.log(fig))
+    
   });
 
 }
